918cefc4cd2734c3c0c9455b7cfcd384c7e823ae
Darkcoder011 <107787867+Darkcoder011@users.noreply.github.com>
2023-03-27 07:57:03 -0700

    Update

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:device_info/device_info.dart';
+import 'package:flutter_styled_toast/flutter_styled_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_nearby_connections/flutter_nearby_connections.dart';
+
+import 'devicesList.dart';
+import 'home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case '/':
+      return MaterialPageRoute(builder: (_) => Home());
+    case 'browser':
+      return MaterialPageRoute(
+          builder: (_) => DevicesListScreen(deviceType: DeviceType.browser));
+    case 'advertiser':
+      return MaterialPageRoute(
+          builder: (_) => DevicesListScreen(deviceType: DeviceType.advertiser));
+    default:
+      return MaterialPageRoute(
+          builder: (_) => Scaffold(
+                body: Center(
+                    child: Text('No route defined for ${settings.name}')),
+              ));
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: generateRoute,
+      initialRoute: '/',
+    );
+  }
+}
